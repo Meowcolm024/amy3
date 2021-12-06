53743c17b41bd2b260a0e4ebfc3d2f216e88aeaa
yky <yuankydyx@outlook.com>
2021-12-06 14:27:32 +0800

    update

--- test/resources/OptGen2.scala
+++ test/resources/OptGen2.scala
@@ -1,8 +1,8 @@
 enum MyBase{
-	case Sub0()
-	case Sub1(x: Int)
-	case Sub2(x: Int, y: Int)
-	case Sub3(s1: MyBase, s2: MyBase)
+  case Sub0()
+  case Sub1(x: Int)
+  case Sub2(x: Int, y: Int)
+  case Sub3(s1: MyBase, s2: MyBase)
 }
 
 def getInt(x: Int): Int = {x}
@@ -10,33 +10,33 @@ def getInt(x: Int): Int = {x}
 @main
 def test(): Unit = {
     val x: MyBase = (
-		MyBase.Sub0();
-		val y: MyBase = MyBase.Sub1(12); 
-		if(15-3 == 12){MyBase.Sub2(15, 16)} 
-		else {y}
-	);
-	val y: MyBase = MyBase.Sub1(10);
-	println(toString(
-		(
+    MyBase.Sub0();
+    val y: MyBase = MyBase.Sub1(12); 
+    if(15-3 == 12){MyBase.Sub2(15, 16)} 
+    else {y}
+  );
+  val y: MyBase = MyBase.Sub1(10);
+  println(toString(
+    (
       MyBase.Sub1(); 
       "Hello"++"World";
       1
     ) 
-		+ 
-		(
+    + 
+    (
       getInt(15); 
       if(!(!(12345;false) || getInt(0) == 2)) {getInt(15)} 
       else {12}; 
       val tmp: Int = 2; 
       tmp
     )
-	));
-	Sub3(y,y) match{
-		case MyBase.Sub2(_, _) => println("Hello1\n")
+  ));
+  Sub3(y,y) match{
+    case MyBase.Sub2(_, _) => println("Hello1\n")
     case MyBase.Sub3(any, _) => any match{
       case Sub2(_, _) => println("Hello2\n")
       case _ => println("Hello3\n")
     }
-		case _ => println("Hello4\n")
-	}
+    case _ => println("Hello4\n")
+  }
 }
